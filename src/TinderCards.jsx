79f49604806641dd96cea729552ed12ce0406974
Hasan1809 <98187639+Hasan1809@users.noreply.github.com>
2025-03-31 23:08:14 +0200

    added link between db and frontend

--- src/TinderCards.jsx
+++ src/TinderCards.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "./axios.js";
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Ahmed Ismail",
-      url: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      name: "Sara Ahmed",
-      url: "image2.png",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+
+      setPeople(req.data);
+    }
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
     console.log(`Removing: ${nameToDelete}, Swipe Direction: ${direction}`);
@@ -35,7 +36,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: "url(" + person.url + ")" }}
+              style={{ backgroundImage: "url(" + person.imgUrl + ")" }}
               className="card"
             >
               <h3>{person.name}</h3>
